49bd9455ae1f23eb984303e507d65c12dbb5021d
NTHanh <taihanh0310@gmail.com>
2017-06-06 07:40:47 +0000

    fixed layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,28 +11,28 @@
         <title>Devices management - @yield('title')</title>
 
         <!-- Bootstrap CSS -->    
-        {!!Html::style("css/bootstrap.min.css")!!}
+        {!! Html::style("css/bootstrap.min.css")!!}
         <!-- bootstrap theme -->
-        {!!Html::style("css/bootstrap-theme.css")!!}
+        {!! Html::style("css/bootstrap-theme.css")!!}
         <!--external css-->
         <!-- font icon -->
-        {!!Html::style("css/elegant-icons-style.css")!!}
-        {!!Html::style("css/font-awesome.min.css")!!}
+        {!! Html::style("css/elegant-icons-style.css")!!}
+        {!! Html::style("css/font-awesome.min.css")!!}
         <!-- full calendar css-->
-        {!!Html::style("assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css")!!}
-        {!!Html::style("assets/fullcalendar/fullcalendar/fullcalendar.css")!!}
+        {!! Html::style("assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css")!!}
+        {!! Html::style("assets/fullcalendar/fullcalendar/fullcalendar.css")!!}
         <!-- easy pie chart-->
-        {!!Html::style("assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css")!!}
+        {!! Html::style("assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css")!!}
         <!-- owl carousel -->
-        {!!Html::style("css/owl.carousel.css")!!}
-        {!!Html::style("css/jquery-jvectormap-1.2.2.css")!!}
+        {!! Html::style("css/owl.carousel.css")!!}
+        {!! Html::style("css/jquery-jvectormap-1.2.2.css")!!}
         <!-- Custom styles -->
-        {!!Html::style("css/fullcalendar.css")!!}
-        {!!Html::style(css/widgets.css")!!}
-        {!!Html::style(css/style.css")!!}
-        {!!Html::style(css/style-responsive.css")!!}
-        {!!Html::style(css/xcharts.min.css")!!}
-        {!!Html::style(css/jquery-ui-1.10.4.min.css")!!}
+        {!! Html::style("css/fullcalendar.css")!!}
+        {!! Html::style("css/widgets.css")!!}
+        {!! Html::style("css/style.css")!!}
+        {!! Html::style("css/style-responsive.css")!!}
+        {!! Html::style("css/xcharts.min.css")!!}
+        {!! Html::style("css/jquery-ui-1.10.4.min.css")!!}
     </head>
 
     <body>
